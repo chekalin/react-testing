dd723d374e79d61c393c76d5036258a79e56fb8e
chekalin <stanislav.chekalin@gmail.com>
2016-04-12 07:09:07 +0100

    comment list stub

--- test/components/comment_list_text.js
+++ test/components/comment_list_text.js
@@ -6,10 +6,20 @@ describe('CommentList', ()=> {
     let component;
 
     beforeEach(() => {
-        component = renderComponent(CommentList);
+        const props = {comments: ['New comment', 'Other new comment']};
+        component = renderComponent(CommentList, null, props);
     });
 
     it('should have comment-list class', () => {
         expect(component).to.have.class('comment-list');
+    });
+
+    it('shows an LI for each element', () => {
+        expect(component.find('li').length).to.equal(2);
+    });
+
+    it('shows each component that is provided', () => {
+        expect(component).to.contain('New comment');
+        expect(component).to.contain('Other new comment');
     });
 });
